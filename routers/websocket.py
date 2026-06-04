c406cc4bff80622e25167efe21d7965a23e1a6ec
ddiazs2512 <ddiazs2512@gmail.com>
2026-06-04 11:22:26 -0500

    websocket.py

--- routers/websocket.py
+++ routers/websocket.py
@@ -2,34 +2,47 @@ from fastapi import APIRouter
 from fastapi import WebSocket
 from fastapi import WebSocketDisconnect
 
+from websocket_manager import manager
+
 router = APIRouter()
 
-@router.websocket("/ws/test")
+@router.websocket("/ws/viaje/{viaje_id}")
 
-async def websocket_test(
-    websocket: WebSocket
+async def websocket_viaje(
+    websocket: WebSocket,
+    viaje_id: int
 ):
 
-    await websocket.accept()
-
-    print("WS conectado")
+    await manager.connect(
+        viaje_id,
+        websocket
+    )
 
     try:
 
         while True:
 
-            data = await websocket.receive_text()
-
-            print(
-                f"WS recibe: {data}"
-            )
-
-            await websocket.send_text(
-                f"echo: {data}"
-            )
+            await websocket.receive_text()
 
     except WebSocketDisconnect:
 
-        print(
-            "WS desconectado"
+        manager.disconnect(
+            viaje_id
         )
+
+@router.get("/ws-test/{viaje_id}")
+
+async def ws_test(
+    viaje_id: int
+):
+
+    await manager.enviar(
+        viaje_id,
+        {
+            "evento": "prueba"
+        }
+    )
+
+    return {
+        "ok": True
+    }
