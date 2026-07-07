87bb5c4141ff2ff31f291520c88574583ee59917
ddiazs2512 <ddiazs2512@gmail.com>
2026-07-07 17:24:04 -0500

    Create viajes.py

---
+++ config/viajes.py
@@ -0,0 +1,9 @@
+# =====================================
+# REGLAS DE NEGOCIO - VIAJES
+# =====================================
+
+PRECIO_MINIMO = 2.00
+PRECIO_MAXIMO = 50.00
+
+DISTANCIA_MINIMA = 100       # metros
+DISTANCIA_MAXIMA = 18000     # 18 km
