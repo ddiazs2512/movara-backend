e11612a03e1ee82ab58b42b033b1090ab89ccdf1
ddiazs2512 <ddiazs2512@gmail.com>
2026-07-09 01:06:49 -0500

    Update places_service.py

--- services/places_service.py
+++ services/places_service.py
@@ -68,60 +68,25 @@ class PlacesService:
         place_id: str,
         session_id: str
     ):
-
+    
         session_token = session_token_service.obtener_session_token(
             session_id
         )
-        
+    
         if not session_token:
             raise Exception("Sesión no encontrada")
-
-        url = (
-            f"https://places.googleapis.com/v1/places/{place_id}"
-            f"?sessionToken={session_token}"
+    
+        data = provider_engine.place_detail(
+            place_id=place_id,
+            session_token=session_token
         )
     
-        headers = {
-            "X-Goog-Api-Key": GOOGLE_PLACES_API_KEY,
-            "X-Goog-FieldMask":
-                "id,displayName,formattedAddress,location"
-        }
-    
-        response = requests.get(
-            url,
-            headers=headers,
-            timeout=10
-        )
-    
-        response.raise_for_status()
-    
-        data = response.json()
-
         session_token_service.eliminar(session_id)
     
         return {
-
+    
             "success": True,
-        
-            "data": {
-        
-                "id":
-                    data.get("id"),
-        
-                "name":
-                    data.get("displayName", {}).get("text"),
-        
-                "address":
-                    data.get("formattedAddress"),
-        
-                "lat":
-                    data.get("location", {}).get("latitude"),
-        
-                "lng":
-                    data.get("location", {}).get("longitude")
-        
-            }
-        
+    
+            "data": data
+    
         }
-
-places_service = PlacesService()
