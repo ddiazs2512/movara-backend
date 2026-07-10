e29e98b8e2840736408cf815588951511f97a928
ddiazs2512 <ddiazs2512@gmail.com>
2026-07-09 20:49:05 -0500

    Update location_bias_builder.py

--- core/places/location_bias_builder.py
+++ core/places/location_bias_builder.py
@@ -14,26 +14,77 @@ class LocationBiasBuilder:
         self,
         lat: float,
         lng: float,
-        radio: int = 10000
+        radio: int = 30000
     ) -> dict:
+    
+        return self.build_bias(
+            lat=lat,
+            lng=lng,
+            radio=radio
+        )
 
+    def build_bias(
+        self,
+        lat: float,
+        lng: float,
+        radio: int
+    ) -> dict:
+    
         return {
-
+    
             "circle": {
-
+    
                 "center": {
-
+    
                     "latitude": lat,
-
+    
                     "longitude": lng
-
+    
                 },
-
+    
                 "radius": float(radio)
-
+    
             }
-
+    
         }
 
+    def build_restriction(
+        self,
+        lat: float,
+        lng: float,
+        radio: int
+    ) -> dict:
+    
+        return {
+    
+            "circle": {
+    
+                "center": {
+    
+                    "latitude": lat,
+    
+                    "longitude": lng
+    
+                },
+    
+                "radius": float(radio)
+    
+            }
+    
+        }
+
+    def build_origin(
+        self,
+        lat: float,
+        lng: float
+    ) -> dict:
+    
+        return {
+    
+            "latitude": lat,
+    
+            "longitude": lng
+    
+        }
 
 location_bias_builder = LocationBiasBuilder()
