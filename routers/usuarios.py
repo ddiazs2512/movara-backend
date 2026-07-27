a235d039a13008f9a5965acec61b5e55aa33c21b
ddiazs2512 <ddiazs2512@gmail.com>
2026-07-27 12:09:34 -0500

    Update usuarios.py

--- routers/usuarios.py
+++ routers/usuarios.py
@@ -174,7 +174,7 @@ def login(usuario: UsuarioLogin, db: Session = Depends(get_db)):
 def obtener_version():
 
     return {
-        "version_minima": 18,
+        "version_minima": 19,
         "forzar": True
     }
 
